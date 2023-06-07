2aa3aa46a5746cf2f3aca1bfed7c305467e0b8ee
Pike1868 <luis_14mendoza@protonmail.com>
2023-06-07 11:45:16 -0400

    Implemented Flask sessions to handle user responses user and survey state

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -7,12 +7,11 @@ app.config['SECRET_KEY'] = 'secret'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-
-responses = []
-
 @app.route("/")
 def show_home_page():
     """Show the user the title of the survey, the instructions, and a button to start the survey. The button should serve as a link that directs the user to /questions/0 (the next step will define that route). """
+    
+    session['testing'] = "Do you see me over there?"
     
     return render_template("index.html")
 
@@ -20,7 +19,7 @@ def show_home_page():
 def show_question(question_index):
     """Show the user the next question in the survey"""
     num_of_questions = len(satisfaction_survey.questions)
-    curr_index = len(responses)
+    curr_index = len(session["responses"])
     if question_index >= num_of_questions:
         return render_template("thanks.html")
     if question_index != curr_index:
@@ -36,6 +35,15 @@ def show_question(question_index):
 def handle_answer(question_index):
     """Handle answer submission and redirect to the next question."""
     answer = request.form["answer"]
-    responses.append(answer)
-    print(responses)
+    answers_list = session["responses"]
+    answers_list.append(answer)
+    session["responses"] = answers_list
+    print(session["responses"], "responses array")
     return redirect(f"/questions/{question_index + 1}")
+
+@app.route("/start" , methods=["POST"])
+def handle_session():
+    session["responses"] = []
+    start_index = len(session["responses"])
+    print(start_index, "start index")
+    return redirect(f"/questions/{start_index}")
